01e9c861dddc63eb68d3246de540cce5efc4bd7b
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2025-06-23 17:34:01 +0530

    Fix Firebase Configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_8E-zXbMAfJFw6QQuceCh864zIVPSAJg",
-  authDomain: "gowda-883c2.firebaseapp.com",
-  databaseURL: "https://gowda-883c2-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "gowda-883c2",
-  storageBucket: "gowda-883c2.firebasestorage.app",
-  messagingSenderId: "322319914242",
-  appId: "1:322319914242:web:8a0b148ac79caf4cb05e1b",
-  measurementId: "G-EDF1KHBY38"
+  apiKey: "AIzaSyA7tsKeb11nY3LZJoqwgs8XPXCQXA24lGA",
+  authDomain: "gowda-b35b4.firebaseapp.com",
+  projectId: "gowda-b35b4",
+  storageBucket: "gowda-b35b4.firebasestorage.app",
+  messagingSenderId: "610973380125",
+  appId: "1:610973380125:web:57c07f32fc2d5376e060f4",
+  measurementId: "G-TLPTP54WFV"
 };
 
 // Initialize Firebase
